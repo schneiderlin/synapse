e1c9fa356690129b854bd5c0ba6dd4e611d3e723
zihao <positivevector@gmail.com>
2026-02-03 20:00:09 +0800

    multi lang wip

--- components/language-learn/src/com/zihao/language_learn/dictionary/core.clj
+++ components/language-learn/src/com/zihao/language_learn/dictionary/core.clj
@@ -1,12 +1,12 @@
 (ns com.zihao.language-learn.dictionary.core
-  (:require 
+  (:require
    [clojure.string :as str]
    [clj-http.client :as http]
    [hickory.core :as h]
    [hickory.select :as s]))
 
-(defn get-translations [word]
-  (let [res (http/get (str "https://en.glosbe.com/id/en/" word))
+(defn get-translations [word source-lang target-lang]
+  (let [res (http/get (str "https://en.glosbe.com/" source-lang "/" target-lang "/" word))
         parsed (h/parse (:body res))
         translations (-> (s/select
                           (s/descendant
@@ -24,15 +24,17 @@
     words))
 
 (comment
-  (get-translations "anjing")
-  (get-translations "mesum")
+  ;; Indonesian to English
+  (get-translations "anjing" "id" "en")
+  (get-translations "mesum" "id" "en")
+
+  ;; Example with different language pairs
+  (get-translations "hello" "en" "es")
+  (get-translations "bonjour" "fr" "en")
 
   ;; 获取基本信息
   (def res (http/get "https://en.glosbe.com/id/en/anjing"))
   (def parsed (h/parse (:body res)))
-  
-
-  
 
   ;; 获取例子
   (def res (http/get "https://en.glosbe.com/id/en/anjing/fragment/tmem?page=1&mode=MUST&stem=true&includedAuthors=&excludedAuthors="))
